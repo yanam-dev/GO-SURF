0c3fa86192dcf5527e8cb046c0b4e9d6ffad5b55
yanam-dev <yamusjko@gmail.com>
2019-10-23 02:02:29 +0200

    added aside navbar menu, double slider in the header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-
+    $('.header__slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<img class="slider-arrow slider-arrow__left" src="images/icons/arrow-left.svg" alt="Link to...">',
+        nextArrow: '<img class="slider-arrow slider-arrow__right" src="images/icons/arrow-right.svg" alt="Link to...">',
+        asNavFor: '.slider-panels'
+      });
+    $('.slider-panels').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
 });
